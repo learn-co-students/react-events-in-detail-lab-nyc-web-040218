b25494d09cd676fc6d1ba84ae51586466e29f734
briecodes <hello@brie.work>
2018-06-05 12:10:16 -0400

    progress.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,16 @@
-// Code CoordinatesButton Component Here
+import React from 'react';
+
+class CoordinatesButton extends React.Component {
+    getCoordinates = (e) => {
+        let x = e.clientX;
+        let y = e.clientY;
+        onReceiveCoordinates(x,y);
+    }
+    render() {
+        return (
+            <button onClick={this.getCoordinates}>Click Me!</button>
+        )
+    }
+}
+
+export default CoordinatesButton;
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,7 @@
-// Code DelayedButton Component Here
+import React from 'react';
+
+class DelayedButton extends React.Component {
+
+}
+
+export default DelayedButton;
